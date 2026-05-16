2857fb4e06b7468bea1aeacefd37f7ec90a2a6f9
Saurav Karn <karnsaurav92@gmail.com>
2026-05-16 22:33:02 +0545

    Add optimized GCD implementation

---
+++ maths/GCD-optimized.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int findGCD(int a, int b){
+
+    while(b != 0){
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
+}
+
+int main(){
+
+    int num1, num2;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "GCD is: " << findGCD(num1, num2);
+
+    return 0;
+}
