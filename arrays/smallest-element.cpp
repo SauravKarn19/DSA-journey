5c009a5f0118efbdeb070cce423a87f8d94ffdbd
Saurav Karn <karnsaurav92@gmail.com>
2026-05-25 21:54:18 +0545

    Added smallest  element array solution

---
+++ arrays/smallest-element.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter array elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int smallest = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] < smallest) {
+            smallest = arr[i];
+        }
+    }
+
+    cout << "Smallest element is: " << smallest << endl;
+
+    return 0;
+}
